fa00cb7800bba318dcbdcd4f7122ead2f1e34327
Eduardo Ismael <eduardo78d@gmail.com>
2016-08-30 10:33:14 -0500

    final forms

--- Project/forms.py
+++ Project/forms.py
@@ -1,18 +1,26 @@
 from wtforms import Form
 from wtforms import StringField, TextField
 from wtforms.fields.html5 import EmailField
-
+from wtforms import HiddenField
 from wtforms import validators
+
+
+def length_honeypot(form, field):
+	if len(field.data) > 0:
+		raise validators.ValidationError('El campo debe estar vacio.')
 
 class CommentForm(Form):
 	username = StringField('username',
-						[ 
-							validators.Required(message = 'El username es requerido!.'),
-							validators.length(min=4, max=25, message='Ingrese un username valido!.'),
-						])
+					[ 
+						validators.Required(message = 'El username es requerido!.'),
+						validators.length(min=4, max=25, message='Ingrese un username valido!.'),
+					])
 	email = EmailField('Correo electronico',
 					[	
 						validators.Required(message = 'El email es requerido!.'),
-						validators.Email(message='Ingre un email valido')]
-					)
+						validators.Email(message='Ingre un email valido'),
+					])
 	comment = TextField('Comentario')
+	honeypot = HiddenField('', [length_honeypot])
+
+
--- Project/main.py
+++ Project/main.py
@@ -4,22 +4,30 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+from flask_wtf.csrf import CsrfProtect
+
 import forms
 
 app = Flask(__name__)
+app.secret_key = 'my_secret_key'
+csrf = CsrfProtect(app)
 
-@app.route('/', methods = ['GET', 'POST'])
-def index():
+@app.route('/cookie')
+def cookie():
+	return render_template('cookie.html')
+
+@app.route('/comment', methods = ['GET', 'POST'])
+def comment():
 	comment_form = forms.CommentForm(request.form)
-	lista = ['Eduardo', 'Ismael', 'Garcia', 'Perez']
-
 	if request.method == 'POST' and comment_form.validate():
 		print comment_form.username.data
 		print comment_form.email.data
 		print comment_form.comment.data
+	else:
+		print "Error en el formulario"
 
 	title = "Curso Flask"
-	return render_template('index.html', title = title, form = comment_form, lista = lista)
+	return render_template('comment.html', title = title, form = comment_form)
 
 if __name__ == '__main__':
 	app.run(debug=True, port=8000)
