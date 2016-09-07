1380318a0d7fd56fed3751f6b899e035f887421c
Eduardo Ismael <eduardo78d@gmail.com>
2016-09-07 09:55:34 -0500

    request

--- Project/main.py
+++ Project/main.py
@@ -5,6 +5,7 @@ from flask import render_template
 from flask import request
 from flask import make_response
 from flask import session
+from flask import flash
 
 from flask import url_for
 from flask import redirect
@@ -15,6 +16,18 @@ import forms
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
 csrf = CsrfProtect(app)
+
+@app.before_request
+def before_request():
+	pass
+
+@app.after_request
+def after_request(response):
+	return response
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
 
 @app.route('/')
 def index():
@@ -30,18 +43,16 @@ def logout():
 		session.pop('username')
 	return redirect(url_for('login'))
 
-@app.route('/cookie')
-def cookie():
-	reponse = make_response( render_template('cookie.html') )
-	reponse.set_cookie('custome_cookie', 'Eduardo')
-	return reponse
-
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
 	login_form = forms.LoginForm(request.form)
 	if request.method == 'POST' and login_form.validate():
+		username = login_form.username.data
+		success_message = 'Bienvenido {}'.format(username)
+		flash(success_message)
+
 		session['username'] = login_form.username.data
-	return render_template('login.html', form = login_form)
+	return render_template('index.html', form = login_form)
 
 @app.route('/comment', methods = ['GET', 'POST'])
 def comment():
@@ -55,6 +66,12 @@ def comment():
 
 	title = "Curso Flask"
 	return render_template('comment.html', title = title, form = comment_form)
+
+@app.route('/cookie')
+def cookie():
+	reponse = make_response( render_template('cookie.html') )
+	reponse.set_cookie('custome_cookie', 'Eduardo')
+	return reponse
 
 if __name__ == '__main__':
 	app.run(debug=True, port=8000)
--- Project/static/js/jquery.js
+++ Project/static/js/jquery.js
@@ -1,3 +1,3 @@
 $(document).ready(function(){
-	console.log("Hola mundo!")
+	console.log("Codigo Facilito")
 });
